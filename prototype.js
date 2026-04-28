44d115f191be6e317248bf45b5ac3443526a60f4
Naveen <naveenyadavpoluboina80@gmail.com>
2026-04-28 16:49:41 +0530

    prototypeconcept file is add

---
+++ prototype.js
@@ -0,0 +1,54 @@
+ /*prototype : Every object has a hidden property called [[Prototype]] accessed via *** Object.getPrototypeOf() or __proto__ ***
+               It acts like a backup object. If JS doesn’t find a property in your object, it looks into its prototype.
+
+               example : 
+
+               const user = {
+                    name: "Mallikharjun"
+                };
+                console.log(user.age); // ❌ undefined
+
+                Now JS checks:
+                Does user have age? → ❌
+                Check prototype → maybe yes
+
+                Add to prototype:
+                const user = {
+                    name: "Mallikharjun"
+                };
+
+                user.__proto__.age = 22;
+                console.log(user.age); // ✅ 22
+                Even though age is NOT in user, it comes from prototype.
+
+                prototype chain: object → prototype → null
+
+                Prototypal Inheritance:
+                One object can inherit properties and methods from another object
+
+                const parent = {
+                    greet() {
+                        console.log("Hello from parent");
+                    }
+                };
+                const child = {};
+                child.__proto__ = parent;
+                child.greet(); // ✅ Hello from parent
+                child doesn’t have greet, but it inherits it.
+
+
+                Prototype with Constructor Functions:
+                function Person(name) {
+                    this.name = name;
+                }
+
+                Person.prototype.sayHello = function() {
+                console.log("Hi, I am " + this.name);
+                };
+
+                const p1 = new Person("Mallikharjun");
+                const p2 = new Person("Ravi");
+
+                p1.sayHello(); // Hi, I am Mallikharjun
+                p2.sayHello(); // Hi, I am Ravi
+*/
