7f8d3923aa9d6fc0938a46cc4c5197560fbd2f93
Naveen <naveenyadavpoluboina80@gmail.com>
2026-05-06 14:58:27 +0530

    optional arguments added to map,filter,some,every methods

--- arrayMethods.js
+++ arrayMethods.js
@@ -1,19 +1,19 @@
 // map method: array method used to create a new array by applying a function to every element in the original array
 let myArray = [1,2,3,4,5];
 let samePower = (each => each*100);
-myArray= myArray.map((each)=> samePower(each));
+myArray= myArray.map((each,currIndex)=> samePower(each)); //currIndex: returns current item index (optional)
 console.log(myArray);//[100,200,300,400,500]
 
 // filter method: an Array method used to create a new array containing only the elements that pass a specific condition
 let arr = [1,2,3,4,-4,,-9];
-let positiveNumbers = arr.filter((eachItem)=> eachItem > 0);
+let positiveNumbers = arr.filter((eachItem,currIndex)=> eachItem > 0); //currIndex: returns current item index (optional)
 console.log(positiveNumbers);//[1,2,3,4]
 
 //reduce method: t is used to reduce an array into a single value.
 /* 
     array.reduce((accumulator, currentValue) => {
     return updatedValue;
-}, initialValue); // initialValue is optional
+}, initialValue); // initialValue is optional 
 | Parameter    | Meaning                               |
 | ------------ | ------------------------------------- |
 | accumulator  | Stores result from previous iteration |
@@ -28,12 +28,12 @@ console.log(borrow);//-1
 
 // every method: check every value follow satified or not return true when every value follow the condition else false
 let arr2 = [1,2,39,5];
-let lessThan40 = arr2.every((each_number) => each_number<40);
+let lessThan40 = arr2.every((each_number,currIndex) => each_number<40); //currIndex: returns current item index (optional)
 console.log(lessThan40); // true 
 
 // some method :he some() method tests whether at least one element in the array passes the test implemented by the provided function. It returns a Boolean value. 
 const myAwesomeArray = ["a", "b", "c", "d", "e"];
-const result = myAwesomeArray.some((alphabet) => alphabet === "d");
+const result = myAwesomeArray.some((alphabet,currIndex) => alphabet === "d"); //currIndex:returns current item index (optional)
 console.log(result); // true
 
 // reverse method: The reverse() method reverses the order of the elements in an array.The first array element becomes the last, and the last array element becomes the first 
